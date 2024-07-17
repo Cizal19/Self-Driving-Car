1297274034ad700ea508021325fe94e42c4beacf
cizal <cizal@esignature.com.np>
2024-07-17 16:21:30 +0545

    Used the created NN to control the car

--- car.js
+++ car.js
@@ -12,8 +12,11 @@ class Car {
     this.angle = 0;
     this.damage = false;
 
+    this.useBrain = controlType == "AI";
+
     if (controlType != "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
     this.controls = new Controls(controlType);
   }
@@ -26,6 +29,17 @@ class Car {
     }
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map(
+        (s) => (s == null ? 0 : 1 - s.offset) // low values if sensor is object is far from the sensor and higher values ~1 if the objects are close
+      );
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain); // neurons of output layer
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
     }
   }
 

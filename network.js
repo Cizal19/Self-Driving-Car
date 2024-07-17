e78dcf854f11ba203f007c89a96e8f009be90f68
cizal <cizal@esignature.com.np>
2024-07-17 15:56:54 +0545

    Created NeuralNetwork Object

--- network.js
+++ network.js
@@ -1,3 +1,20 @@
+class NeuralNetwork {
+  constructor(neuronCounts) {
+    this.levels = [];
+    for (let i = 0; i < neuronCounts.length; i++) {
+      this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+    }
+  }
+
+  static feedForward(givenInputs, network) {
+    let outputs = Level.feedForward(givenInputs, network.levels[0]);
+    for (let i = 1; i < network.levels.length; i++) {
+      outputs - Level.feedForward(outputs, network.levels[i]);
+    }
+    return outputs;
+  }
+}
+
 class Level {
   constructor(inputCount, outputCount) {
     this.inputs = new Array(inputCount);
@@ -15,7 +32,7 @@ class Level {
   static #randomize(level) {
     for (let i = 0; i < level.inputs.length; i++) {
       for (let j = 0; j < level.outputs.length; j++) {
-        level.weights[i][j] = Math.random() * 2 - 1;
+        level.weights[i][j] = Math.random() * 2 - 1; // [-1,1]
       }
     }
 

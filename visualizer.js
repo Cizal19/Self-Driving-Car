6b2bafd6ed2b067252bac9e593c5e134d57c4164
cizal <cizal@esignature.com.np>
2024-07-17 21:03:13 +0545

    Added animation to the NN visualization

--- visualizer.js
+++ visualizer.js
@@ -6,10 +6,36 @@ class Visualizer {
     const width = ctx.canvas.width - margin * 2;
     const height = ctx.canvas.height - margin * 2;
 
-    Visualizer.drawLevel(ctx, network.levels[0], left, top, width, height);
+    const levelHeight = height / network.levels.length;
+    for (let i = network.levels.length - 1; i >= 0; i--) {
+      const levelTop =
+        top +
+        lerp(
+          height - levelHeight,
+          0,
+          network.levels.length == 1 ? 0.5 : i / (network.levels.length - 1)
+        );
+      ctx.setLineDash([7, 3]);
+      Visualizer.drawLevel(
+        ctx,
+        network.levels[i],
+        left,
+        levelTop,
+        width,
+        levelHeight,
+        i == network.levels.length - 1
+          ? [
+              String.fromCharCode(8679),
+              String.fromCharCode(8678),
+              String.fromCharCode(8680),
+              String.fromCharCode(8681),
+            ]
+          : []
+      );
+    }
   }
 
-  static drawLevel(ctx, level, left, top, width, height) {
+  static drawLevel(ctx, level, left, top, width, height, outputLabels) {
     const right = left + width;
     const bottom = top + height;
 
@@ -60,6 +86,18 @@ class Visualizer {
       ctx.setLineDash([3, 3]);
       ctx.stroke();
       ctx.setLineDash([]);
+
+      if (outputLabels[i]) {
+        ctx.beginPath();
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "white";
+        ctx.font = nodeRadius * 1 + "px Arial";
+        ctx.fillText(outputLabels[i], x, top + nodeRadius * 0.1);
+        ctx.lineWidth = 0.5;
+        ctx.strokeText(outputLabels[i], x, top + nodeRadius * 0.1);
+      }
     }
   }
 
